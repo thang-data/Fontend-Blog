f747dbe1dc11927d072d9bb20c3d75424a124d8d
Nguyen Duc Thang <thang-nd@blockhive.vn>
2022-08-05 16:35:05 +0700

    update

---
+++ src/components/global/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="text-center bg-light py-4">
+      <h6>Chào mừng tới với trang luật của Hạnh</h6>
+      <p> Copyright &copy; 2021</p>
+    </div>
+  )
+}
+
+export default Footer
